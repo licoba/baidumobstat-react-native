2fe06e1a694999173aae59b8c8807921d1c5851a
licoba <licoba@163.com>
2021-06-19 16:16:45 +0800

    添加Log导包

--- android/src/main/java/com/baidu/reactnativemobstat/RNBaiduMobStatModule.java
+++ android/src/main/java/com/baidu/reactnativemobstat/RNBaiduMobStatModule.java
@@ -10,7 +10,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
-
+import android.util.Log;
 import java.util.HashMap;
 import java.util.Map;
 
